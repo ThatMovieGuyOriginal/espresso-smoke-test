3216c9bfe70cea8e8b6208e23c34164942919fc3
Josh Hickman <joshua.alan.hickman@gmail.com>
2025-12-04 00:39:31 -0700

    feat: add broken heater coil image to agitation section

--- components/Agitation.tsx
+++ components/Agitation.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import Image from 'next/image'
+
 export default function Agitation() {
   return (
     <section className="container-custom py-20 md:py-32 border-t border-dark-border">
@@ -37,6 +41,17 @@ export default function Agitation() {
               Vacuum breakers fail slowly. By the time you hear the hiss, moisture is damaging your logic board.
             </p>
           </div>
+        </div>
+
+        {/* Visual: Broken Heater Coil */}
+        <div className="mb-12 flex justify-center">
+          <Image
+            src="/broken-heater-coil.png"
+            alt="Corroded and broken heater coil showing scale buildup damage"
+            width={400}
+            height={300}
+            className="rounded-lg shadow-xl border border-dark-border"
+          />
         </div>
 
         <div className="bg-dark-surface border-l-4 border-accent-orange rounded-lg p-8">
